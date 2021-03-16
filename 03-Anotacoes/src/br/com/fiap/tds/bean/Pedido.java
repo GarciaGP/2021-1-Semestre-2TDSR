ee9175b94ed49dcad7908443362d8565d006f18a
Thiago Yamamoto <thiagoyama@Thiagos-MacBook-Pro.local>
2021-03-16 20:24:14 -0300

    Anotações

---
+++ 03-Anotacoes/src/br/com/fiap/tds/bean/Pedido.java
@@ -0,0 +1,42 @@
+package br.com.fiap.tds.bean;
+
+import br.com.fiap.tds.annotation.Coluna;
+
+public class Pedido {
+	
+	@Coluna(nome="cd_pedido")
+	private int codigo;
+	
+	@Coluna(nome="vl_pedido")
+	private double valor;
+	
+	@Coluna(nome="ds_pedido", tamanho = 100)
+	private String descricao;
+	
+	//CTRL + 3 -> ggas
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+}
